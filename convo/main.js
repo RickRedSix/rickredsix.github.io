08966d6143a56aad964e2589a5cbb1ff598b0620
Rick Smith <ricks@twitter.com>
2020-04-09 13:14:52 +0100

    adding convo and gitignore

---
+++ convo/main.js
@@ -0,0 +1,33 @@
+//Menu Buttons
+/*
+function bindMenuButtons() {
+    
+    var linkElements = document.querySelectorAll('.scroll-link');    
+    
+    for(var i = 0; i < linkElements.length; i++) {
+                
+        linkElements[i].addEventListener("click", function () {            
+            
+            var elementToScrollTo = this.dataset.scrollto;            
+                        
+            document.querySelector('.' + elementToScrollTo).scrollIntoView({ 
+              behavior: 'smooth' 
+            });
+            
+        });
+        
+    } 
+                                  
+};
+
+bindMenuButtons();
+*/
+
+function init() {
+  alert('Convo Test');  
+};
+
+function init()
+
+
+
