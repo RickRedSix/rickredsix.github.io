fc3487275a5b6e345a23cc54a2dfeb654f7dcb6f
Rick Smith <ricks@twitter.com>
2020-04-09 13:57:35 +0100

    finished demo

--- convo/main.js
+++ convo/main.js
@@ -23,8 +23,64 @@ function bindMenuButtons() {
 bindMenuButtons();
 */
 
-function init() {
-  console.log('Convo Test');  
+function init() {  
+  
+    var checkbox1 = document.querySelector('#checkbox-1'),
+        checkbox2 = document.querySelector('#checkbox-2'),
+        checkbox3 = document.querySelector('#checkbox-3'),
+        checkbox4 = document.querySelector('#checkbox-4'),
+        submitButton = document.querySelector('#button');    
+        
+        
+    submitButton.addEventListener('click', function () {
+        
+        var tweetURLBase = 'https://twitter.com/intent/tweet?text='
+        
+        var tweetText = 'I chose '
+        
+        if(checkNumberOfChoices() === 0) {
+            alert('Please choose at least one option')
+            return
+        }
+        
+        if(checkbox1.checked)
+            tweetText += '#Option1 '
+            
+        if(checkbox2.checked)
+            tweetText += '#Option2 '
+            
+        if(checkbox3.checked)
+            tweetText += '#Option3 '
+            
+        if(checkbox4.checked)
+            tweetText += '#Option4 '                   
+            
+        tweetText += 'https://cards.twitter.com/cards/3jhkt3/9c4jz'
+        
+        tweetURLBase += encodeURIComponent(tweetText)
+        
+        window.open(tweetURLBase, '_blank');
+                        
+    })  
+    
+    function checkNumberOfChoices() {
+        
+        var count = 0;
+        
+        if(checkbox1.checked)
+            count++
+            
+        if(checkbox2.checked)
+            count++
+            
+        if(checkbox3.checked)
+            count++
+            
+        if(checkbox4.checked)
+            count++                                    
+            
+        return count             
+    }               
 };
 
 init();
