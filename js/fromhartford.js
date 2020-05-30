6b2500828fa3f18c3697cb0cb78e1d3a773bdd30
Rick Smith <ricks@twitter.com>
2020-05-30 17:40:29 +0100

    chanigng test js

--- js/fromhartford.js
+++ js/fromhartford.js
@@ -1,5 +1,14 @@
-function () {
+function() {
     
-  console.log("I'm Here!");
+    console.log("I'm Here!");
+  
+	var imageLinks = document.getElementsByClassName('image-link');
+  
+	console.log(imageLinks.length)
+	
+
+    for (var i = 0; i < imageLinks.length; i++) { 
+            console.log(imageLinks[i]); 
+    } 			
   
 }()
